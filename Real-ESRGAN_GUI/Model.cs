f2be9650a8fec2d71f785b58dd937432d9cc580a
net2cn <mcopener@gmail.com>
2021-09-07 19:38:24 +0800

    Fix image with alpha channel not scaling properly.
    I'll add additional process for alpha channel later.
    
    Signed-off-by: net2cn <mcopener@gmail.com>

--- Real-ESRGAN_GUI/Model.cs
+++ Real-ESRGAN_GUI/Model.cs
@@ -4,9 +4,8 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using Microsoft.ML;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 
@@ -14,14 +13,15 @@ namespace Real_ESRGAN_GUI
 {
     public class Model : IDisposable
     {
-        private string modelPath = "";
+        private string modelName = "";
         private InferenceSession session;
         private Logger logger = Logger.Instance;
 
         public async Task LoadModel(string modelPath, string modelName)
         {
-            if (session == null || this.modelPath != modelPath)
+            if (session == null || this.modelName != modelName)
             {
+                this.modelName = modelName;
                 session = await Task.Run(() => { return new InferenceSession(Path.Combine(modelPath, $"{modelName}.onnx")); });
             }
         }
@@ -29,6 +29,12 @@ namespace Real_ESRGAN_GUI
         public async Task Scale(string inputPath, string outputPath, string outputFormat, int scale)
         {
             Bitmap image = new Bitmap(inputPath);
+            if (IsAlphaBitmap(image))
+            {
+                image = RemoveAlphaChannel(image);
+            }
+            //TODO: Add Alpha channel inference.
+
 
             logger.Log("Creating input image...");
             var inMat = ConvertImageToFloatTensorUnsafe(image);
@@ -42,8 +48,9 @@ namespace Real_ESRGAN_GUI
             image = ConvertFloatTensorToImageUnsafe(outMat);
 
             var saveName = Path.GetFileName(inputPath);
-            logger.Log($"Writing image to {outputPath}{saveName.Split(".")[0]}_{scale}x.{outputFormat} ...");
-            image.Save($"{outputPath}{saveName.Split(".")[0]}_{scale}x.{outputFormat}");
+            var savePath = $"{outputPath}{saveName.Split(".")[0]}_{modelName}.{outputFormat}";
+            logger.Log($"Writing image to {savePath}...");
+            image.Save(savePath);
             logger.Progress += 10;
         }
 
@@ -55,12 +62,12 @@ namespace Real_ESRGAN_GUI
             return (Tensor<float>)output;
         }
 
-        public Tensor<float> ConvertImageToFloatTensorUnsafe(Bitmap image)
+        public static Tensor<float> ConvertImageToFloatTensorUnsafe(Bitmap image)
         {
             // Create the Tensor with the appropiate dimensions for the NN
             Tensor<float> data = new DenseTensor<float>(new[] { 1, 3, image.Height, image.Width });
 
-            BitmapData bmd = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, image.PixelFormat);
+            BitmapData bmd = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, image.PixelFormat);
             int PixelSize = 3;
 
             unsafe
@@ -72,9 +79,9 @@ namespace Real_ESRGAN_GUI
                     for (int x = 0; x < bmd.Width; x++)
                     {
                         // note the order of colors is BGR, convert to RGB
-                        data[0, 0, x, y] = row[x * PixelSize + 2] / (float)255.0;
-                        data[0, 1, x, y] = row[x * PixelSize + 1] / (float)255.0;
-                        data[0, 2, x, y] = row[x * PixelSize + 0] / (float)255.0;
+                        data[0, 0, x, y] = row[(x * PixelSize) + 2] / (float)255.0;
+                        data[0, 1, x, y] = row[(x * PixelSize) + 1] / (float)255.0;
+                        data[0, 2, x, y] = row[(x * PixelSize) + 0] / (float)255.0;
                     }
                 }
 
@@ -83,7 +90,7 @@ namespace Real_ESRGAN_GUI
             return data;
         }
 
-        public Bitmap ConvertFloatTensorToImageUnsafe(Tensor<float> tensor)
+        public static Bitmap ConvertFloatTensorToImageUnsafe(Tensor<float> tensor)
         {
             Bitmap bmp = new Bitmap(tensor.Dimensions[2], tensor.Dimensions[3], PixelFormat.Format24bppRgb);
             BitmapData bmd = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp.PixelFormat);
@@ -107,6 +114,37 @@ namespace Real_ESRGAN_GUI
                 bmp.UnlockBits(bmd);
             }
             return bmp;
+        }
+
+        public static bool IsAlphaBitmap(Bitmap bitmap)
+        {
+            BitmapData bmd = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
+            unsafe
+            {
+                byte* ptrAlpha = ((byte*)bmd.Scan0.ToPointer()) + 3;
+                for (int i = bmd.Width * bmd.Height; i > 0; --i)  // prefix-- should be faster
+                {
+                    if (*ptrAlpha < 255)
+                    {
+                        bitmap.UnlockBits(bmd);
+                        return false;
+                    }
+                    ptrAlpha += 4;
+                }
+            }
+            bitmap.UnlockBits(bmd);
+            return true;
+        }
+
+        public static Bitmap RemoveAlphaChannel(Bitmap bitmap)
+        {
+            Bitmap target = new Bitmap(bitmap.Width, bitmap.Height, PixelFormat.Format24bppRgb);
+            target.SetResolution(bitmap.HorizontalResolution, bitmap.VerticalResolution);   // Set both bitmap to same dpi to prevent scaling.
+            Graphics g = Graphics.FromImage(target);
+            g.Clear(Color.White);
+            g.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
+            g.DrawImage(bitmap, 0, 0);
+            return target;
         }
 
         public void Dispose()
