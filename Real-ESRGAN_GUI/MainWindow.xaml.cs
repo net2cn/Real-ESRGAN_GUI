c38c4aae14b9417dcf877b5c6a83ec3e6a7c37a5
net2cn <mcopener@gmail.com>
2022-04-30 16:33:53 +0800

    Fix output file is saved to the parent directory when using file path as input.
    NC
    
    Signed-off-by: net2cn <mcopener@gmail.com>

--- Real-ESRGAN_GUI/MainWindow.xaml.cs
+++ Real-ESRGAN_GUI/MainWindow.xaml.cs
@@ -102,6 +102,7 @@ namespace Real_ESRGAN_GUI
             {
                 // inputPath is a file.
                 files.Add(inputPath);
+                inputPath = Path.GetDirectoryName(inputPath);
             }
             Logger.Progress = 10;
 
